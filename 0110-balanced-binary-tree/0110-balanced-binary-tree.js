526b41f05766ec88897ec4cfb0162bd12931c7b3
Soohan Kim <kimsoohan.dev@gmail.com>
2025-01-23 22:41:36 +0900

    Time: 129 ms (5.04%), Space: 60.7 MB (5.14%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+    if (root === null) {
+        return true;
+    }
+
+    const getDepth = (root) => {
+        if (root === null) {
+            return 0;
+        }
+
+        let leftDepth = getDepth(root.left);
+        let rightDepth = getDepth(root.right);
+
+        if (leftDepth === -1 || rightDepth === -1) {
+            return -1;
+        }
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            return -1;
+        }
+        console.log("left, right", leftDepth, rightDepth, root);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    };
+
+    if (getDepth(root) === -1) {
+        return false;
+    }
+
+    return true;
+};
