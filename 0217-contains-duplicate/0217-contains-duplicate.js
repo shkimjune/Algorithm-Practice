01cbf606ff1e9a2d7c31995841dd401f97c6c955
Soohan Kim <kimsoohan.dev@gmail.com>
2025-02-04 13:19:24 +0900

    Time: 9 ms (89.01%), Space: 63.6 MB (19.08%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numsSet = new Set();
+
+    for (const num of nums) {
+        if (numsSet.has(num)) {
+            return true;
+        }
+
+        numsSet.add(num);
+    }
+
+    return false;
+};
