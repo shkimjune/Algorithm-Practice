a0672d92707de440eb682bf1e1364102bed43b22
Soohan Kim <kimsoohan.dev@gmail.com>
2025-03-14 10:16:13 +0900

    Time: 27 ms (17.67%), Space: 65.9 MB (14.66%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -8,10 +8,7 @@ var MinStack = function () {
  * @return {void}
  */
 MinStack.prototype.push = function (val) {
-    this.stack.push({
-        val: val,
-        min: this.stack.length === 0 ? val : Math.min(val, this.getMin()),
-    });
+    this.stack.push(val);
 };
 
 /**
@@ -25,14 +22,19 @@ MinStack.prototype.pop = function () {
  * @return {number}
  */
 MinStack.prototype.top = function () {
-    return this.stack[this.stack.length - 1].val;
+    return this.stack[this.stack.length - 1];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-    return this.stack[this.stack.length - 1].min;
+    const min = this.stack.reduce(
+        (minimum, number) => Math.min(minimum, number),
+        this.stack[0],
+    );
+
+    return min;
 };
 
 /** 
