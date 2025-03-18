e5b32e8fc90fa99930bfdd2e44acd61cc4aab3a7
Soohan Kim <kimsoohan.dev@gmail.com>
2025-03-18 20:34:32 +0900

    Time: 0 ms (100%), Space: 57.4 MB (30.52%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    const res = new ListNode();
+    let currentNode = res;
+
+    while (list1 && list2) {
+        if (list1.val > list2.val) {
+            currentNode.next = list2;
+            list2 = list2.next;
+        } else {
+            currentNode.next = list1;
+            list1 = list1.next;
+        }
+
+        currentNode = currentNode.next;
+    }
+
+    if (list1 !== null) {
+        currentNode.next = list1;
+    } else if (list2 !== null) {
+        currentNode.next = list2;
+    }
+
+    return res.next;
+};
