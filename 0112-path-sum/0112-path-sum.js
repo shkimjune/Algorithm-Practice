16d6e05ca1acd3167da140cca8e39fdd487b7c02
Soohan Kim <kimsoohan.dev@gmail.com>
2025-04-15 23:54:15 +0900

    Time: 0 ms (100%), Space: 59.1 MB (26.39%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+    if (!root) {
+        return false;
+    }
+
+    if (!root.left && !root.right && targetSum === root.val) {
+        return true;
+    }
+
+    targetSum -= root.val;
+
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+};
