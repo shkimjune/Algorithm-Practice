0dd61883205730665cf17aab61f3f384e95859e5
Soohan Kim <kimsoohan.dev@gmail.com>
2025-06-28 16:42:31 +0900

    Time: 0 ms (100%), Space: 55.3 MB (98.53%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if (!root) {
+        return 0;
+    }
+
+    let sum = 0;
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const node = queue.shift();
+
+        if (node.left) {
+            if (!node.left.left && !node.left.right) {
+                sum += node.left.val;
+            } else {
+                queue.push(node.left);
+            }
+        }
+
+        if (node.right) {
+            queue.push(node.right);
+        }
+    }
+
+    return sum;    
+};
